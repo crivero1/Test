84d183aa4423bc10f8797497387656202391e812
Cristian Riveros <criveros@ing.puc.cl>
2014-03-11 10:05:06 -0400

    Second Commit

--- src/test/code/init.java
+++ src/test/code/init.java
@@ -3,7 +3,7 @@ package test.code;
 public class init {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test");
+		System.out.println("This is the second test!!");
 	}
 
 }
